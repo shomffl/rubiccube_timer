36f5fb59eb32721fab4e425c06de81360e747559
shomffl <showatanabe0226@gmail.com>
2021-12-12 22:54:04 +0900

    corsの追加

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 base_dir = os.path.abspath(os.path.dirname(__name__))
+
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
