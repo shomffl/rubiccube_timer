312a92ae501a3064696095c81c7d598d2fc5c12e
shomffl <showatanabe0226@gmail.com>
2021-12-13 13:16:26 +0900

    ファイル名の変更

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../logo.svg";
 import "../App.css";
 
-export const Home = () => {
+export const Login = () => {
   return (
     <div className="App">
       <header className="App-header">
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../components/Home";
+import { Login } from "../components/Login";
 
 export const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
