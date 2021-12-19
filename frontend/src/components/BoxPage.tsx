98b269250c4ce021b3cdb40fbb58116b9c2c8680
shomffl <showatanabe0226@gmail.com>
2021-12-19 14:45:45 +0900

    ボックス削除処理を実装

--- frontend/src/components/BoxPage.tsx
+++ frontend/src/components/BoxPage.tsx
@@ -3,34 +3,59 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const BoxPage = () => {
-  const [boxList, setBoxList] = useState<string[]>([]);
+  const [boxList, setBoxList] = useState<String[]>([]);
+  const [selectKey, setSelectKey] = useState<String>();
+  const [boolList, setBoolList] = useState<boolean[]>([]);
   const navigate = useNavigate();
-
 
   useEffect(() => {
     const getUserName = localStorage.getItem("username");
     const data = { username: getUserName };
-    axios
-      .post("/get_box_list", data)
-      .then((res) => setBoxList(res.data.box_list));
+    axios.post("/get_box_list", data).then((res) => {
+      setBoxList(res.data.box_list);
+      setBoolList([...Array(res.data.box_num)].map(() => true));
+    });
   }, []);
 
-  
-
+  const onClickDelete = (e: any) => {
+    const getUserName = localStorage.getItem("username");
+    const data = {
+      deleteKey: selectKey,
+      username: getUserName,
+    };
+    axios.post("/delete_box", data).then((res) => {
+      setBoxList(res.data.box_list);
+      setBoolList([...Array(res.data.box_num)].map(() => true));
+    });
+  };
 
   return (
     <div>
       <button onClick={(e) => navigate("/timer")}>back</button>
-      <table cellSpacing="10vw">
-        <tr><td>番号</td><td>平均</td></tr>
-        {boxList.map((box, index) => (
-          <tr>
-            <td>{index + 1}</td>
-            <td>{box[1]}</td>
-            <button>選択</button>
-            <button >削除</button>
-          </tr>
-        ))}
+      <table>
+        <tbody>
+          {boxList.map((box, index) => (
+            <tr key={index} id={box[0]}>
+              <td>{index + 1}</td>
+              <td>{box[1]}</td>
+              <td>
+                <button
+                  onClick={(e) => {
+                    setSelectKey(box[0]);
+                    boolList[index] = false;
+                  }}
+                >
+                  選択
+                </button>
+              </td>
+              <td>
+                <button onClick={onClickDelete} disabled={boolList[index]}>
+                  削除
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
